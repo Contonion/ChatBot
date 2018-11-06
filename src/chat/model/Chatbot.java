c9ead261dc582ae81137ec7c150d0b515c6f9e0b
Cade Jackman <engineer202@outlook.com>
2018-11-06 09:04:01 -0700

    Update src/chat/model/Chatbot.java

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -4,65 +4,80 @@ import java.util.ArrayList;
 
 public class Chatbot
 {
-	ArrayList responseList = new ArrayList();
-	ArrayList spookyList = new ArrayList();
+	ArrayList responseList = new ArrayList<String>();
+	ArrayList spookyList = new ArrayList<String>();
 	private String joke;
 	private String currentUser;
 	private String content;
 	private String getContent;
 	private String contentChecker;
-	public Chatbot() {
+
+	public Chatbot()
+	{
 		this.joke = "Why did the monkey cross the road? It was stapled to the chicken";
 		this.currentUser = new String("Default User - Boring!!");
 		this.content = new String("Empty of all content but not null");
-		
+
 		this.responseList = new ArrayList<String>();
 		this.spookyList = new ArrayList<String>();
 		buildTheLists();
 	}
-	private void buildTheLists() {
+
+	private void buildTheLists()
+	{
 		responseList.add("Hello! How are you?");
 		responseList.add("Goodbye");
 		responseList.add("Yes");
 		responseList.add("No");
 		responseList.add("What is your name??????????????????????????????????????????????????TELLLLLLMEEEEEEE");
-		
+
 		spookyList.add("Halloween");
 		spookyList.add("Christmas is better than Halloween");
 		spookyList.add("Boo");
 		spookyList.add("Turn around...");
 	}
-	public boolean contentChecker(String text) {
+
+	public boolean contentChecker(String text)
+	{
 		boolean hasContent = false;
-		if (text.equals(content)) {
+		if (text.equals(content))
+		{
 			hasContent = true;
-		} else if(text.equals(null)) {
-				hasContent = false;
-		} else {
+		}
+		else if (text.equals(null))
+		{
+			hasContent = false;
+		}
+		else
+		{
 			hasContent = false;
 		}
 		return hasContent;
 	}
+
 	public String processText(String userText)
 	{
 		String answer = "";
 		answer += "You said " + userText;
-		if (contentChecker(userText)) {
+		if (contentChecker(userText))
+		{
 			answer += "You said the special words. \n";
 		}
 		return answer;
-		
+
 	}
+
 	public boolean sentimentChecker(String text)
 	{
 		boolean hasSentiment = false;
 		return hasSentiment;
 	}
+
 	public boolean legitimacyChecker(String input)
 	{
 		boolean isValid = true;
-		
-		if(input == null)
+
+		if (input == null)
 		{
 			isValid = false;
 		}
@@ -76,6 +91,7 @@ public class Chatbot
 		}
 		return isValid;
 	}
+
 	public boolean spookyChecker(String input)
 	{
 		boolean isSpooky = false;
@@ -86,20 +102,25 @@ public class Chatbot
 		{
 			
 		}
+		public String getContent() {
+			return Content;
+		}
 	
 		
 		
 	}
-	public getSpookyList() {
-		ArrayList spookyList = new ArrayList();
-		return getSpookyList();
-	}
-	public void getContent() {
+	public void getSpookyList() 
+	{
 		
 	}
-	public void getResponseList() {
-		
+	public void getContent()
+	{
+
 	}
-	
-	
+
+	public void getResponseList()
+	{
+
+	}
+
 }
